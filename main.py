67f7cd64773268817a03067d0ef1f4c90e997a98
Blackstar-0 <61096474+Blackstar-0@users.noreply.github.com>
2022-07-20 17:22:47 +0200

    final form

---
+++ main.py
@@ -0,0 +1,18 @@
+from model.rubrica import *
+
+contatti = rubrica()
+contatti.aggiungereContatto("Bolivar","boli", "5555")
+contatti.aggiungereContatto("Arsene","arse", "2222")
+b=contatti.listaContatti()
+for i in b:
+    print(i.telefono)
+contatti.rimuoviContatto(0)
+b=contatti.listaContatti()
+for i in b:
+    print(i)
+
+
+
+
+
+
---
+++ model/rubrica.py
@@ -0,0 +1,36 @@
+class rubrica:
+    def __init__(self):
+        self.contatto=[]
+
+    def aggiungereContatto(self, nome, cognome, telefono):
+        new_contatto = Contatto(nome, cognome, telefono)
+        self.contatto.append(new_contatto)
+
+    def rimuoviContatto(self, position):
+        self.contatto.remove(self.contatto[position])
+
+
+    def cercaContatto(self, key):
+        #key=nome
+        for i in self.contatto:
+            if i.nome == key:
+                return i
+        pass
+
+    def stampaContatto(self, position):
+        print(self.contatto[position])
+
+
+    def listaContatti(self):
+        return self.contatto
+
+
+
+class Contatto:
+    def __init__(self, nome, cognome, telefono):
+        self.nome = nome
+        self.cognome = cognome
+        self.telefono = telefono
+
+    def __str__(self):
+        return self.nome + ' ' + self.cognome + ' ' + self.telefono
