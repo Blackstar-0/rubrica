e955082878eb594ad5b38464f5683aad1a3764bd
Blackstar-0 <61096474+Blackstar-0@users.noreply.github.com>
2022-07-20 17:57:42 +0200

    r

--- main.py
+++ main.py
@@ -10,32 +10,3 @@ contatti.rimuoviContatto(0)
 b=contatti.listaContatti()
 for i in b:
     print(i)
-
-
-
-
-
-
-=======
-=======
->>>>>>> 91271cffbf578ed074556f45dce78035b35e24bc
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-<<<<<<< HEAD
->>>>>>> 91271cffbf578ed074556f45dce78035b35e24bc
-=======
->>>>>>> 91271cffbf578ed074556f45dce78035b35e24bc
