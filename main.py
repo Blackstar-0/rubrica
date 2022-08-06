4c87f9b6aa26f1962bd7eea7528d3e728aed52ec
Blackstar-0 <61096474+Blackstar-0@users.noreply.github.com>
2022-08-06 09:31:46 +0200

    Revert "sanita final end"
    This reverts commit e3884b1bee7ed08c0f72d51a6bb611d6c97684dd.

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 from model.rubrica import *
-import mysql.connector
-from model.sanita import *
-
-
-connection = mysql.connector.connect(host='localhost',
-                                         database='palestra',
-                                         user='root',
-                                         password='root')
-
-
-#rubrica test
 
 contatti = rubrica()
 contatti.aggiungereContatto("Bolivar","boli", "5555")
@@ -21,22 +10,3 @@ contatti.rimuoviContatto(0)
 b=contatti.listaContatti()
 for i in b:
     print(i)
-
-
-
-#Sanita test
-
-sanita = Sanita()
-
-sanita.aggiungiDottore("bolivar","arsene","codiceFiscale",2039)
-sanita.aggiungiDottore("bobo","gean","codiceFiscale1",2045)
-sanita.aggiungiDottore("made","sees","codiceFiscale2",2023)
-sanita.aggiungiPaziente("nugu","web","fiscalcodice1",1009)
-sanita.aggiungiPaziente("elge","debe","fiscalcodice2",1065)
-sanita.aggiungiPaziente("zcbe","mbuito","fiscalcodice3",1098)
-
-sanita.assegnaMedico(2039,1009)
-print(sanita.getPazienti(2039))
-print(sanita.getDottore(2039))
-print(sanita.getPaziente(1009))
-sanita.salvaPazienti()
